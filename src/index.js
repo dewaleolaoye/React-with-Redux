09ddbf133a3550ad4972521de0067366d368a90e
dewaleolaoye <adewaleolaoye21@gamil.com>
2018-12-29 15:28:20 +0100

    jsx javascript variables

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = function() {
+  const buttonText = "Click Me";
   return (
     <div>
-      <label>Enter your name: </label>
-      <input type="text" className="" />
+      <label className="label" forHTML="name">
+        Enter your name:{" "}
+      </label>
+      <input id="name" type="text" />
       <button type="submit" style={{ backgroundColor: "blue", color: "white" }}>
-        Submit
+        {buttonText}
       </button>
     </div>
   );
